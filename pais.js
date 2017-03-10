0b46f7f223784acbcd724df569c7c743759574e7
thassiov <tvmcarvalho@gmail.com>
2017-03-10 20:21:05 -0300

    Fazendo request para a API

--- pais.js
+++ pais.js
@@ -6,6 +6,10 @@
 'use strict';
 
 const readline = require('readline');
+const http = require('http');
+const API = {
+    url: 'http://api.worldbank.org/countries/',
+    format: '?format=json'};
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -16,6 +20,17 @@ const msg = 'Insira o código do país a ser verificado (ISO 3166-1 alpha-2 code
 rl.question(msg, function(ans) {
     rl.close();
     const pais = ans.toLowerCase().trim();
-    console.log(pais);
+    http.get(API.url + pais + API.format, function(res) {
+        let finalData = '';
+
+        res.on('data', function(d) {
+            finalData +=d;
+        });
+
+        res.on('end', function() {
+            finalData = JSON.parse(finalData);
+            console.log(finalData);
+        });
+    });
 });
 
